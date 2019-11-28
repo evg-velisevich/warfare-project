83814070d8cf1a2389731b91a44d5cc8a1b93cf6
evg-velisevich <evg.velisevich@gmail.com>
2019-11-28 22:53:00 +0500

    Немного методов

--- engine/Script.php
+++ engine/Script.php
@@ -1,13 +1,140 @@
 <?php
 
-class Script extends Game
+class Script
 {
 
-    const VK_GAME = "okop";
-    const OK_GAME = "ok_okop";
-    const MAIL_GAME = "mail_okop";
-    const BEGIN_DIV_NAME = "Начальный дивизион";
     protected $userModel = [];
+
+    private $auth_key = '',
+            $user_id = '',
+            $ch,
+            $active_net;
+
+    /**
+     * Constructor.
+     * @param $id
+     * @param $auth
+     * @throws Exception
+     */
+    function __construct($id, $auth)
+    {
+        if (!$this->ch) $this->ch = curl_init();
+        if (!$id || !$auth) {
+            throw new Exception('Not have some data');
+        } else {
+            $this->setUserID($id);
+            $this->setAuthKey($auth);
+        }
+    }
+
+    /**
+     * @param $user_id
+     */
+    function setUserID($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @param $net
+     */
+    public function setActiveNet($net)
+    {
+        $this->active_net = $net;
+    }
+
+    /**
+     * @param $userID
+     * @return array
+     */
+    public function socialGet($userID)
+    {
+        $data = ['friends' => json_encode([(string)$userID])];
+        return $this->gameApi('social_get', $data);
+    }
+
+    /**
+     * @param $package
+     * @param $params
+     * @return array
+     */
+    public function gameApi($package, $params)
+    {
+        $url = 'http://' . $this->getServer() . '/' . $this->getUserID() . '/' . $this->getAuthKey() . '/' . $package;
+        return $this->curlRequest($url, $params);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getServer()
+    {
+        $servers = [
+            'vk' => '5.178.83.9' . rand(1, 2),
+            'fb' => '95.213.136.62',
+            'ok' => '31.131.250.243',
+            'mail' => '95.213.136.61'
+        ];
+        return $servers[$this->getActiveNet()];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActiveNet()
+    {
+        return $this->active_net;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserID()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    /**
+     * @param $auth_key
+     */
+    public function setAuthKey($auth_key)
+    {
+        $this->auth_key = $auth_key;
+    }
+
+    /**
+     * @param $url
+     * @param $data
+     * @return array
+     */
+    public function curlRequest($url, $data)
+    {
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, 0);
+        curl_setopt($this->ch, CURLOPT_HEADER, 0);
+        curl_setopt($this->ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U;Windows NT 5.1;en-US; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3');
+        curl_setopt($this->ch, CURLOPT_POST, 1);
+        curl_setopt($this->ch, CURLOPT_POSTFIELDS, is_array($data) ? http_build_query($data) : $data);
+        $res = [
+            json_decode(curl_exec($this->ch), true),
+            curl_getinfo($this->ch)
+        ];
+        return $res;
+    }
+
+    public function isCorrectPack($pack, $userID)
+    {
+        return (is_array($pack) && $this->getValue($pack, [0, 0, 0]) === $userID);
+    }
 
     /**
      * @param int $achievementIndex
@@ -31,16 +158,15 @@ class Script extends Game
             'time' => 0,
         ];
 
-        if (
-            ($data = $this->getValue($array, [$achievementCategory, $achievementIndex]) && $achievementCategory > 0)
-            || ($data = $this->getValue($array, [$achievementIndex]) && $achievementCategory < 0)
-        ) {
-            if (is_array($data)) {
+        if ($achievementCategory > 0) {
+            if ($data = $this->getValue($array, [$achievementCategory, $achievementIndex])) {
                 $result = [
                     'index' => $data[0],
                     'time' => $data[1],
                 ];
-            } else if (is_numeric($data) || is_string($data)) {
+            }
+        } else {
+            if ($data = $this->getValue($array, [$achievementIndex])) {
                 $result = [
                     'index' => 0,
                     'time' => $data,
@@ -59,9 +185,6 @@ class Script extends Game
      */
     public function getValue(array $array, $key, $default = null)
     {
-        if ($key instanceof Closure) {
-            return $key($array, $default);
-        }
         if (is_array($key)) {
             $lastKey = array_pop($key);
             foreach ($key as $keyPart) {
@@ -123,17 +246,18 @@ class Script extends Game
 
     /**
      * @param int $achievementIndex
+     * @param int $achievementCategory
      * @return array
      */
-    public function getCompletedCategorizedAchievementData(int $achievementIndex): array
+    public function getCompletedCategorizedAchievementData(int $achievementIndex, int $achievementCategory): array
     {
-        return $this->getAchievementData($this->getCompletedCategorizedAchievements(), $achievementIndex);
+        return $this->getAchievementData($this->getCompletedCategorizedAchievements(), $achievementIndex, $achievementCategory);
     }
 
     /**
      * @return array
      */
-    private function getCompletedCategorizedAchievements(): array
+    public function getCompletedCategorizedAchievements(): array
     {
         return $this->getValue($this->getUserModel(), 'completed_categorized_achievements', []);
     }
@@ -219,47 +343,61 @@ class Script extends Game
     }
 
     /**
-     * @return int
+     * @return array
      */
-    public function getBossLimit(): int
+    public function getBossLimit(): array
     {
-        $limit = 0;
+        $limit = [
+            'limit' => 0,
+            'guild_boost' => 0,
+            'territories_boost' => 0,
+            'status_boost' => 0,
+            'talents_boost' => 0,
+            'hero_boost' => 0
+        ];
 
         if ($this->getMaxLevel() >= 3) {
-            $limit = 15;
+            $limit['limit'] = 15;
 
-            $guildLevelsBoost = ['10' => 20, '15' => 30, '27' => 40, '45' => 50, '55' => 60];
-            foreach ($guildLevelsBoost as $boost) {
-                if ($this->getUserGuildLevel() >= $boost) {
-                    $limit += $boost;
+            $guildLevelsBoost = [10 => 20, 15 => 30, 27 => 40, 45 => 50, 55 => 60];
+            foreach ($guildLevelsBoost as $level => $boost) {
+                if ($this->getUserGuildLevel() >= $level) {
+                    $limit['limit'] += $boost;
+                    $limit['guild_boost'] += $boost;
                 }
             }
 
-            $statusLevelsBoost = ['15' => 20, '13' => 15, '10' => 12, '7' => 8, '4' => 5];
-            foreach ($statusLevelsBoost as $boost) {
-                if ($this->getStatusLevel() >= $boost) {
-                    $limit += $boost;
-                    break;
+            if ($this->isActiveStatus()) {
+                $statusLevelsBoost = [15 => 20, 13 => 15, 10 => 12, 7 => 8, 4 => 5];
+                foreach ($statusLevelsBoost as $level => $boost) {
+                    if ($this->getStatusLevel() >= $level) {
+                        $limit['limit'] += $boost;
+                        $limit['status_boost'] += $boost;
+                        break;
+                    }
                 }
             }
 
             $talentsBoost = [72, 49, 31];
             foreach ($talentsBoost as $boost) {
-                $limit += $this->getTalent($boost);
+                $limit['limit'] += $this->getTalent($boost);
+                $limit['talents_boost'] += $this->getTalent($boost);
             }
 
-            $territoriesBoost = $this->getAchievementData($this->getCompletedCategorizedAchievements(), 1, 14);
+            $territoriesBoost = $this->getCompletedCategorizedAchievementData(14, 1);
 
             if ($territoriesBoost['index'] > 0) {
-                $limit += $territoriesBoost['index'] * 5;
+                $limit['limit'] += $territoriesBoost['index'] * 5;
+                $limit['territories_boost'] = $territoriesBoost['index'] * 5;
             }
 
             $heroBoost = [0, 1, 2, 3, 14, 15, 13, 7, 4];
 
             foreach ($heroBoost as $boost) {
-                $boostData = $this->getAchievementData($this->getCompletedAchievements(), $boost);
-                if ($boostData['time'] <= time()) {
-                    $limit += 5;
+                $boostData = $this->getCompletedAchievementData($boost);
+                if ($boostData['index'] === 0 && $boostData['time'] <= time()) {
+                    $limit['limit'] += 5;
+                    $limit['hero_boost'] += 5;
                 }
             }
         }
@@ -353,7 +491,38 @@ class Script extends Game
      */
     public function getStatusTime(): int
     {
-        return $this->getValue($this->getStatus(), 'status_time', 0);
+        return $this->getValue($this->getInterimResources(), 'status_time', 0);
+    }
+
+    /**
+     * @return array
+     */
+    public function renderHtml (): array
+    {
+        $html = [];
+
+        $html['level'] = $this->getMaxLevel();
+        $html['achievement_points'] = $this->getAchievementsPoints();
+        $html['boss_limit'] = $this->getBossLimit();
+        $html['status']['time'] = $this->getStatusInfo();
+        $html['experience'] = $this->getExperience();
+        $html['guild']['level'] = $this->getUserGuildLevel();
+        $html['guild']['id'] = $this->getUserGuildID();
+        $html['talents'] = $this->getUsedTalents();
+        $html['army'] = $this->getArmyStrength();
+        $html['status']['level'] = $this->getStatusLevel();
+        $html['srut'] = $this->getSrutAmount();
+        $html['last_login'] = $this->getLastLogin();
+
+        return $html;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastLogin(): int
+    {
+        return $this->getValue($this->getUserModel(), 'last_login', -1);
     }
 
     /**
@@ -410,7 +579,7 @@ class Script extends Game
     /**
      * @return string
      */
-    private function getSquadName(): string
+    public function getSquadName(): string
     {
         return $this->getValue($this->getNames(), 'squad_name', 'Мой отряд');
     }
@@ -418,7 +587,7 @@ class Script extends Game
     /**
      * @return array
      */
-    private function getNames(): array
+    public function getNames(): array
     {
         return $this->getValue($this->getUserModel(), 'names', []);
     }
@@ -427,7 +596,7 @@ class Script extends Game
      * @param int $number
      * @return string
      */
-    private function numFormat(int $number): string
+    public function numFormat(int $number): string
     {
         return number_format($number, 0, '', ' ');
     }
@@ -436,7 +605,7 @@ class Script extends Game
      * @param int $timestamp
      * @return string
      */
-    private function ts2date(int $timestamp): string
+    public function ts2date(int $timestamp): string
     {
         return date("d.m.Y в H:i:s", $timestamp);
     }
